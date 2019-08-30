d4a37ab81f1cb792cb3fad875c4b145dc1ca5d5b
blake <blake.netzeband@spiredigital.com>
2019-08-30 11:11:39 -0600

    updated templates

---
+++ express/data/mi.js
@@ -0,0 +1,18 @@
+module.exports = {
+  items: [
+    {
+      number: 1,
+      location: "See site inspection map for location",
+      comments: "Sign posted at entrance of site.",
+      dateInit: "01/01/19",
+      dateCompleted: "01/01/19",
+    },
+    {
+      number: 6,
+      location: "near bridge",
+      comments: "maint item #3",
+      dateInit: "08/29/19",
+      dateCompleted: "Not completed",
+    },
+  ],
+};
