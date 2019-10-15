df49b1649b04b194edd8d492f174ec4eddd0c620
blake <blake.netzeband@spiredigital.com>
2019-10-15 11:31:35 -0600

    added screenshot data

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const caContext = require("../data/ca");
 const miContext = require("../data/mi");
 const certContext = require("../data/cert");
+const screenshotContext = require("../data/screenshot");
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", certContext);
+  res.render("index", screenshotContext);
 });
 
 module.exports = router;
